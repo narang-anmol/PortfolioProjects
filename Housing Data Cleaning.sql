f13d70943070da5c7d41442cc47c4cef1aa48e38
narang-anmol <155387021+narang-anmol@users.noreply.github.com>
2024-01-19 15:11:00 -0500

    Update Housing Data Cleaning.sql

--- Housing Data Cleaning.sql
+++ Housing Data Cleaning.sql
@@ -23,9 +23,7 @@
 --Breaking out Address Into Individual Columns (Address, City, State)
 
 	ALTER TABLE nashvillehousing
-	ADD propertysplitaddress VARCHAR(255);
-
-	ALTER TABLE nashvillehousing
+	ADD propertysplitaddress VARCHAR(255),
 	ADD propertysplitcity VARCHAR(255);
 	
 	UPDATE nashvillehousing
@@ -33,5 +31,58 @@
 	propertysplitaddress = SUBSTRING(propertyaddress, 1, POSITION(',' IN propertyaddress) -1),
 	propertysplitcity = SUBSTRING(propertyaddress, POSITION(',' IN propertyaddress) +1, LENGTH(propertyaddress));
 
+--Breaking out Owner Address Into Individual Columns (Address, City, State)
 
---TO BE UPDATED
+	ALTER TABLE nashvillehousing
+	ADD ownersplitaddress VARCHAR(255),
+	ADD ownersplitcity VARCHAR(255),
+	ADD ownersplitstate VARCHAR(255);
+
+	UPDATE nashvillehousing
+	SET 
+	ownersplitaddress = split_part(replace(owneraddress, ',', '.'), '.', 1),
+    ownersplitcity = split_part(replace(owneraddress, ',', '.'), '.', 2),
+    ownersplitstate = split_part(replace(owneraddress, ',', '.'), '.', 3);
+	
+--Change Y and N to Yes and No for Uniformity in "Sold As Vacant" Coluumn
+
+	/* SELECT COUNT(*), soldasvacant
+	FROM nashvillehousing
+	GROUP BY soldasvacant */
+	
+	UPDATE nashvillehousing
+	SET soldasvacant = 
+					CASE 
+                    WHEN soldasvacant = 'Y' THEN 'Yes'
+                    WHEN soldasvacant = 'N' THEN 'No'
+                    ELSE soldasvacant
+                  	END;
+					
+--Removing Duplicate Rows
+
+	WITH RowNumCTE AS (
+	SELECT *,
+        ROW_NUMBER() OVER (
+            PARTITION BY parcelid,
+                         propertyaddress,
+                         saleprice,
+                         saledate,
+                         legalreference
+            ORDER BY UniqueID
+						) row_num
+    FROM nashvillehousing
+		)
+	DELETE
+	FROM nashvillehousing
+	USING RowNumCTE
+	WHERE nashvillehousing.uniqueid = RowNumCTE.uniqueid AND row_num > 1;
+	
+--Deleting Unused Columns
+
+	ALTER TABLE nashvillehousing
+	DROP COLUMN owneraddress,
+	DROP COLUMN taxdistrict,
+	DROP COLUMN propertyaddress,
+	DROP COLUMN saledate;
+
+	--SELECT * FROM nashvillehousing
